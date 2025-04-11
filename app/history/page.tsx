0cfd0971e5f044e785a9a4c1a6c94703527655a7
asraful islam <asrafulislamrayhan2@gmail.com>
2025-04-11 16:25:20 +0600

    added new sitemap

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,7 +1,9 @@
 import { AppShell } from "@/components/app-shell";
 import { HistoryPage } from "@/components/history-page";
 import Loader from "@/components/Loader";
+
 import { Suspense } from "react";
+
 
 export default function History() {
   return (
